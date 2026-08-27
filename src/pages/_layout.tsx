1521a7a9265fc90009cc86200c1302cfce819c78
ra1nyxin <224879924+ra1nyxin@users.noreply.github.com>
2026-08-27 19:00:55 +0800

    删除侧栏顶部品牌标志

--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -18,7 +18,6 @@ import {
   Menu,
   MenuItem,
   Paper,
-  SvgIcon,
   ThemeProvider,
 } from '@mui/material'
 import dayjs from 'dayjs'
@@ -28,9 +27,6 @@ import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Outlet, useNavigate } from 'react-router'
 
-import iconDark from '@/assets/image/icon_dark.svg?react'
-import iconLight from '@/assets/image/icon_light.svg?react'
-import LogoSvg from '@/assets/image/logo.svg?react'
 import { BaseErrorBoundary } from '@/components/base'
 import { LayoutItem } from '@/components/layout/layout-item'
 import { NoticeManager } from '@/components/layout/notice-manager'
@@ -112,7 +108,6 @@ const OS = getSystem()
 
 const Layout = () => {
   const mode = useThemeMode()
-  const isDark = mode !== 'light'
   const { t } = useTranslation()
   const { theme } = useCustomTheme()
   const { verge, mutateVerge, patchVerge } = useVerge()
@@ -314,30 +309,6 @@ const Layout = () => {
 
         <div className="layout-content">
           <div className="layout-content__left">
-            <div className="the-logo" data-tauri-drag-region="false">
-              <div
-                data-tauri-drag-region="true"
-                style={{
-                  height: '27px',
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                }}
-              >
-                <SvgIcon
-                  component={isDark ? iconDark : iconLight}
-                  style={{
-                    height: '36px',
-                    width: '36px',
-                    marginTop: '-3px',
-                    marginRight: '5px',
-                    marginLeft: '-3px',
-                  }}
-                  inheritViewBox
-                />
-                <LogoSvg fill={isDark ? 'white' : 'black'} />
-              </div>
-            </div>
-
             {menuUnlocked && (
               <Box
                 sx={(theme) => ({
